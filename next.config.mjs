c6a4152469aca9fd2f32cad6fd9c8a4b09bdbe95
Hector Berrutti <82546028+TitoBerru@users.noreply.github.com>
2025-01-03 19:11:56 -0300

    next.config deje solo el archivo mjs por posibles conflictos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 export default nextConfig;
